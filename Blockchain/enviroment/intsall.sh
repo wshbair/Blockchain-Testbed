a56b62928862d74a6ec4555575e00e7ade4fbb50
Wazen Shbair <engwazen@gmail.com>
2017-07-02 22:29:55 +0200

    Delete intsall.sh

--- Blockchain/enviroment/intsall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# installing ethereum
-sudo apt-get install software-properties-common
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo apt-get update
-sudo apt-get install -y ethereum
