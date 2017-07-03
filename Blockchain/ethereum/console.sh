407342770f4b7e58598b2239169acea499cfc8c4
Wazen Shbair <engwazen@gmail.com>
2017-07-03 10:57:21 +0200

    Create console.sh

---
+++ Blockchain/ethereum/console.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Open Console window
+geth --identity "LuxBch" --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30303" --rpcapi "db,eth,net,web3" --networkid 3576 --nat "any"  console
