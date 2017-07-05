9ea0e86b718ca84cc87df27e6a4b78baf2bc85b4
Wazen Shbair <engwazen@gmail.com>
2017-07-05 10:48:43 +0200

    Update install.sh

--- Blockchain/ethereum/install.sh
+++ Blockchain/ethereum/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # installing ethereum and docker
+sudo apt-get update
 sudo apt-get -y install python-software-properties
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:ethereum/ethereum
