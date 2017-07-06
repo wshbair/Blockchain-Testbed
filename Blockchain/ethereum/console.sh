babc8c7fabb53bd825e3257880ea51832519c922
Wazen Shbair <engwazen@gmail.com>
2017-07-06 16:10:16 +0200

    Update console.sh

--- Blockchain/ethereum/console.sh
+++ Blockchain/ethereum/console.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Open Console window
-nohup geth --identity "LuxBch" --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30303" --rpcapi "db,eth,net,web3" --networkid 3576 --nat "any"
+nohup geth --identity "LuxBch" --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30303" --rpcapi "db,eth,net,web3" --networkid 3576 --nat "any" > /dev/null 2>&1 &
