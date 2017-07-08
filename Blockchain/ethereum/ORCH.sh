8e4cdc7c0f6837b1e6d3dcccd22631ac26a94018
Wazen Shbair <engwazen@gmail.com>
2017-07-08 10:58:49 +0200

    Update ORCH.sh

--- Blockchain/ethereum/ORCH.sh
+++ Blockchain/ethereum/ORCH.sh
@@ -5,4 +5,27 @@ for host in `cat REMOTEHOST`; do
  scp xp-bootstrap washbair@$host
  nohup ssh washbair@$host "ruby xp-bootstrap --reserve" /dev/null 2>&1 &
  done
-end
+echo "Collect infromation"
+#Collect information 
+for host in `cat REMOTEHOST`; do
+ remotenodes=`ssh washbair@$host 'cat nodes.txt'`
+ for node in $remotenodes; do
+  echo $node$'\r'>>nodes.txt
+  done
+  echo `ssh washbair@$host 'cat static-nodes.json'`>>remotenodes
+ done
+ localstnode=`cat static-nodes.json`
+ rmstnode=`cat remotenodes`
+ echo '['$localstnode$','$rmstnode$']'>>static-nodes.json
+
+filename="nodes.txt"
+while read -r line
+do
+    scp "static-nodes.json" root@$line:/home/luxbch/data
+done < $filename
+
+for host in `cat $filename`; do
+    ssh  root@$host  pkill geth
+    ssh  root@$host  Blockchain-Testbed/Blockchain/ethereum/mining.sh 2> /dev/null 2>&1 &
+    echo done node $host
+done
