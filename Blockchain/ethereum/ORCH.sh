dc6039fe610d7968f1ea63089ad1668385fad226
Wazen Shbair <engwazen@gmail.com>
2017-07-08 11:30:25 +0200

    Update ORCH.sh

--- Blockchain/ethereum/ORCH.sh
+++ Blockchain/ethereum/ORCH.sh
@@ -3,6 +3,7 @@
 for host in `cat REMOTEHOST`; do
  #Copy reservation script to remote node
  scp xp-bootstrap washbair@$host:/home/washbair
+ ssh washbair@host gem install --user-install ruby-cute
  nohup ssh washbair@$host "ruby xp-bootstrap --reserve" /dev/null 2>&1 &
  done
 echo "Collect infromation"
