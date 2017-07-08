1544d09e9571b2babc196a5d7b90d42e5b7c2176
Wazen Shbair <engwazen@gmail.com>
2017-07-08 11:26:30 +0200

    Update ORCH.sh

--- Blockchain/ethereum/ORCH.sh
+++ Blockchain/ethereum/ORCH.sh
@@ -2,7 +2,7 @@
 # Send reservation for remote nodes
 for host in `cat REMOTEHOST`; do
  #Copy reservation script to remote node
- scp xp-bootstrap washbair@$host
+ scp xp-bootstrap washbair@$host:/home/washbair
  nohup ssh washbair@$host "ruby xp-bootstrap --reserve" /dev/null 2>&1 &
  done
 echo "Collect infromation"
