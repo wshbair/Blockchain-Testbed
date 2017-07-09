c4e5f2a7d01574ff0cab6a2c82dd4387cbe1bf0f
Wazen Shbair <engwazen@gmail.com>
2017-07-09 12:55:17 +0200

    Update Blockchain.sh

--- Blockchain/ethereum/Blockchain.sh
+++ Blockchain/ethereum/Blockchain.sh
@@ -1,32 +1,25 @@
-#GRID5000 stuff
-#!/bin/bash
+#function join { local IFS="$1"; shift; echo "$*"; }
+#array=()
+#for host in `cat REMOTEHOST`; do
+ #echo $host
+ #remotenodes=`ssh washbair@$host 'cat nodes.txt'`
+ #echo $remotenodes
+  #for node in $remotenodes; do
+  # echo $node$'\r'>>nodes.txt
+   #done
+# x=`ssh washbair@$host 'cat static-nodes.json'`
+ #array+=($x)
+ #done
+ #z=` join , ${array[@]}`
 
-function join { local IFS="$1"; shift; echo "$*"; }
+# localstnode=`cat static-nodes.json`
+# echo '['$localstnode$','$z$']'>static-nodes.json
 
-array=()
-for host in `cat REMOTEHOST`; do
- echo $host
- remotenodes=`ssh washbair@$host 'cat nodes.txt'`
- echo $remotenodes
-  for node in $remotenodes; do
-   echo $node$'\r'>>nodes.txt
-   done
- x=`ssh washbair@$host 'cat static-nodes.json'`
- array+=($x)
- done
- z=` join , ${array[@]}`
+filename="nodes.txt"
 
- localstnode=`cat static-nodes.json`
- echo '['$localstnode$','$z$']'>static-nodes.json
-
-#filename="nodes.txt"
-#while read -r line
-#do
-#    scp "static-nodes.json" root@$line:/home/luxbch/data
-#done < $filename
-
-#for host in `cat $filename`; do
-#    ssh  root@$host  pkill geth
-#    ssh  root@$host  Blockchain-Testbed/Blockchain/ethereum/mining.sh 2> /dev/null 2>&1 &
-#    echo done node $host
-#done
+for host in `cat $filename`; do
+    scp -oStrictHostKeyChecking=no "static-nodes.json" root@$host:/home/luxbch/data #2> /dev/null 2>&1 &
+    ssh  root@$host  pkill geth
+    ssh  root@$host  Blockchain-Testbed/Blockchain/ethereum/mining.sh 2> /dev/null 2>&1 &
+    #echo done node $host
+done
