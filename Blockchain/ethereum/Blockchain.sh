278b9bbed589f0f99e5479a8d5783c8e9b6fba3e
Wazen Shbair <engwazen@gmail.com>
2017-07-10 17:38:40 +0200

    Update Blockchain.sh

--- Blockchain/ethereum/Blockchain.sh
+++ Blockchain/ethereum/Blockchain.sh
@@ -20,6 +20,6 @@ filename="nodes.txt"
 for host in `cat $filename`; do
     scp -oStrictHostKeyChecking=no "static-nodes.json" root@$host:/home/luxbch/data #2> /dev/null 2>&1 &
     ssh  root@$host  pkill geth
-    ssh  root@$host  Blockchain-Testbed/Blockchain/ethereum/mining.sh 2> /dev/null 2>&1 &
+    ssh  root@$host  Blockchain-Testbed/Blockchain/ethereum/console.sh 2> /dev/null 2>&1 &
     #echo done node $host
 done
