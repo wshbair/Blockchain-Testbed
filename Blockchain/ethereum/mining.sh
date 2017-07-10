3a564f7b1c9bfc753caa9b1198f7729b225c01a3
Wazen Shbair <engwazen@gmail.com>
2017-07-10 16:13:02 +0200

    Update mining.sh

--- Blockchain/ethereum/mining.sh
+++ Blockchain/ethereum/mining.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Start mining
-nohup geth --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30313" --nodiscover --networkid 3576 --nat "any" --mine --minerthreads=8 > /dev/null 2>&1 &
+nohup geth --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30313" --nodiscover --networkid 3576 --nat "any" --unlock 0 --password<(echo -n "") --mine --minerthreads=8 > /dev/null 2>&1 &
