cb95c765475e041dc8594ccffa55f50375ece9a0
Wazen Shbair <engwazen@gmail.com>
2017-07-10 18:22:37 +0200

    Update init.sh

--- Blockchain/ethereum/init.sh
+++ Blockchain/ethereum/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Create User
-nohup geth --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30303" --nodiscover --networkid 3576 --nat "none" --password <(echo -n "") account new > /dev/null 2>&1 &
+#nohup geth --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30303" --nodiscover --networkid 3576 --nat "none" --password <(echo -n "") account new > /dev/null 2>&1 &
 #Set Genensis File
 nohup geth --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30303" --nodiscover --networkid 3576 --nat "none" init "CommonGenesis.json" > /dev/null 2>&1 &
