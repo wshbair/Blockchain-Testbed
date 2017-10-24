42172def044ab6b8db0f63f32bb31782eedbdada
Wazen Shbair <engwazen@gmail.com>
2017-10-24 14:11:34 +0200

    Update Blockchain.sh

--- G5K-Deployment-Tool/Blockchain.sh
+++ G5K-Deployment-Tool/Blockchain.sh
@@ -1,9 +1,33 @@
+clear
 filename="nodes.txt"
-echo 'Transfering static-nodes to network nodes'
+#echo 'Transfering static-nodes to network nodes'
+#for host in `cat $filename`; do
+#    ssh  root@$host  pkill geth
+#    sleep 5
+#   scp -oStrictHostKeyChecking=no "static-nodes.json" root@$host:/home/luxbch/data #2> /dev/null 2>&1 &
+#    nohup ssh  root@$host  Blockchain-Testbed/Blockchain/ethereum/mining.sh 2> /dev/null 2>&1 &
+#done
+#sleep 60
+#echo "SlEEPING 60sec to make node have some Ethers for next Workloads transactions"
+
+echo "All nodes ready and run in mining mode"
+echo "------------------------------------------------------"
+echo "Blockchain Workload #1"
 for host in `cat $filename`; do
-    ssh  root@$host  killall -KILL geth
-    scp -oStrictHostKeyChecking=no "static-nodes.json" root@$host:/home/luxbch/data #2> /dev/null 2>&1 &
-    ssh  root@$host Blockchain-Testbed/Blockchain/ethereum/mining.sh 2> /dev/null 2>&1 &
-    echo done node $host
+     echo CPUheavy on $host
+     ssh  root@$host  nodejs Blockchain-Testbed/Blockchain/ethereum/cpuheavy.js 10
 done
-#echo "All nodes ready and run in mining mode"
+echo "------------------------------------------------------"
+echo "Blockchain Workload #2"
+for host in `cat $filename`; do
+     echo IOHeavy on $host
+     ssh  root@$host  nodejs Blockchain-Testbed/Blockchain/ethereum/ioheavy/deploy.js
+     contractAddress=$(ssh  root@$host  nodejs Blockchain-Testbed/Blockchain/ethereum/ioheavy/deploy.js 2>&1)
+     echo $contractAddress
+     echo $contractAddress
+     ssh  root@$host  nodejs Blockchain-Testbed/Blockchain/ethereum/ioheavy/write.js 10 10 111 $contractAddress
+     ssh  root@$host  nodejs Blockchain-Testbed/Blockchain/ethereum/ioheavy/scan.js 10 10 111 $contractAddress
+     echo "******"
+
+done
+echo "------------------------------------------------------"
