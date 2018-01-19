cc92aadf8c48177b01fc7238d0ef7b33c20e3f60
Wazen Shbair <engwazen@gmail.com>
2018-01-19 23:55:41 +0100

    Update mining.sh

--- G5K-Deployment-Tool/mining.sh
+++ G5K-Deployment-Tool/mining.sh
@@ -1,9 +1,6 @@
 clear
 filename="nodes.txt"
 for host in `cat $filename`; do
-   #ssh  root@$host  pkill geth
-   #sleep 5
-   #scp -oStrictHostKeyChecking=no "static-nodes.json" root@$host:/home/luxbch/data #2> /dev/null 2>&1 &
    nohup ssh  root@$host  Blockchain-Testbed/Blockchain/ethereum/mining.sh 2> /dev/null 2>&1 &
 done
 echo "SlEEPING 100sec to make node have some Ethers for next Workloads transactions"
