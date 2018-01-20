c73b356a9636e898e2cef273f2c3f2d7143f25c7
Wazen Shbair <engwazen@gmail.com>
2018-01-20 22:25:11 +0100

    Update and rename node-reservation.rb to G5KNodeReservation.rb

--- G5K-Deployment-Tool/G5KNodeReservation.rb
+++ G5K-Deployment-Tool/G5KNodeReservation.rb
@@ -1,22 +1,25 @@
 #!/usr/bin/ruby -w
 require 'cute'
 g5k = Cute::G5K::API.new()
-
-time='1:00:00'
-nodes_number=2
-nodes=""
+nodes_number=Integer(ARGV[0])
+time=ARGV[1]
+g5knodes=Array.new
+jobs=Array.new
 sites = g5k.site_uids
 job = {}
+
 sites.each do |site|
-  job = g5k.reserve(:nodes => nodes_number, :site => site, :walltime => time, :wait => false, :env => 'ubuntu1404-x64-min', :type => :deploy)
+  job = g5k.reserve(:nodes => nodes_number, :site => site, :walltime => time, :wait => false, :type => :deploy)
+  jobs.push(job)
   begin
     job = g5k.wait_for_job(job, :wait_time => 60)
     x=job['assigned_nodes']
-    nodes=nodes+x
-  rescue  Cute::G5K::EventTimeout
-    puts "We waited too long in site #{site} let's release the job and try in another site"
-    g5k.release(job)
-  end
+    g5k.deploy(job, :env => 'ubuntu1404-x64-min', :wait => true)
+    g5knodes=g5knodes+x
+    rescue  Cute::G5K::EventTimeout
+      puts "We waited too long in site #{site} let's release the job and try in another site"
+      g5k.release(job)
+    end
 end
 
 puts "All nodes are reserved"
@@ -27,9 +30,14 @@ File.open("nodes.txt", "w+") do |f|
   f.puts(nodes)
 end
 
+#save jobs id
+File.open("jobs.txt", "w+") do |x|
+  x.puts(jobs) 
+end
+
 output = File.open( "static-nodes.json","w" )
 nodeslink=[]
-puts "Running commands"
+puts "Installing Ethereurm Blockchain on all nodes"
 
 ssh = Net::SSH::Multi::Session::new
 nodes.each { |n| ssh.use "root@#{n}" }
@@ -40,7 +48,7 @@ nodes.each { |n| ssh.use "root@#{n}" }
  ssh.exec!("Blockchain-Testbed/Blockchain/ethereum/install.sh")
  ssh.exec!("Blockchain-Testbed/Blockchain/ethereum/init.sh")#creat a user and run Gensis command
 
-puts "Generate static node address"
+puts "Generating static-node address"
 # We needd to get the address of the static node
 output = File.open( "static-nodes.json","w" )
 nodeslink=[]
@@ -55,4 +63,4 @@ for @hostname in nodes
 end
 output<<'[ '+nodeslink.join(',')+' ]'
 output.close
-puts "Experiment preparation finished."
+puts "Experiment preparation finished"
