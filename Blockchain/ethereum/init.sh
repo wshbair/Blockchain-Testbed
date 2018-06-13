83f26a0a9479bfe08610905d8c9de500aab26f1b
Wazen Shbair <engwazen@gmail.com>
2018-06-13 16:04:47 +0200

    Update init.sh

--- Blockchain/ethereum/init.sh
+++ Blockchain/ethereum/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Set Genensis File
-ETH_DATA='Blockchain-Testbed/Blockchain/ethereum/'
+ETH_DATA='/root/Blockchain-Testbed/Blockchain/ethereum/'
 #rm -fr /home/luxbch/data/geth
 #nohup geth --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30303" --nodiscover --networkid 3576 --nat "none" init "CommonGenesis.json" 2> /dev/null 2>&1 &
 #nohup geth --rpc --rpcport "8084" --rpccorsdomain "*" --datadir "/home/luxbch/data" --port "30303" --nodiscover --networkid 3576 --nat "none" init $ETH_DATA"_"$1".json" 2> /dev/null 2>&1 &
