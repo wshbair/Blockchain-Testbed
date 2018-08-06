e6efbf9ad48489e337916711573645c7da9777d9
Wazen Shbair <engwazen@gmail.com>
2018-08-06 15:18:46 +0200

    Update Loads.sh

--- Blockchain/ethereum/smallbank/Loads.sh
+++ Blockchain/ethereum/smallbank/Loads.sh
@@ -1,30 +1,30 @@
 echo '10 txRate ------------------------------------------------------'
-./driver  -ops 10000 -threads 4 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 20000 -threads 4 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 30000 -threads 4 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 40000 -threads 4 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 50000 -threads 4 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 4 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 6 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 8 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 10 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 12 -txrate 10 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
 echo '20 txRate ------------------------------------------------------'
-./driver  -ops 10000 -threads 4 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 20000 -threads 4 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 30000 -threads 4 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 40000 -threads 4 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 50000 -threads 4 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 4 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 6 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 8 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 10 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 12 -txrate 20 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
 echo '30 txRate ------------------------------------------------------'
-./driver  -ops 10000 -threads 4 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 20000 -threads 4 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 30000 -threads 4 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 40000 -threads 4 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 50000 -threads 4 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 4 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 6 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 8 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 10 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 12 -txrate 30 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
 echo '40 txRate ------------------------------------------------------'
-./driver  -ops 10000 -threads 4 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 20000 -threads 4 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 30000 -threads 4 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 40000 -threads 4 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 50000 -threads 4 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 4 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 6 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 8 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 10 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 2000 -threads 12 -txrate 40 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
 echo '50 txRate ------------------------------------------------------'
 ./driver  -ops 10000 -threads 4 -txrate 50 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 20000 -threads 4 -txrate 50 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 30000 -threads 4 -txrate 50 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 40000 -threads 4 -txrate 50 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
-./driver  -ops 50000 -threads 4 -txrate 50 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 20000 -threads 6 -txrate 50 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 30000 -threads 8 -txrate 50 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 40000 -threads 10 -txrate 50 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
+./driver  -ops 50000 -threads 12 -txrate 50 -fp stat.txt -endpoint 127.0.0.1:8084 -db ethereum
