545b5821462f1f19b901ea367f41624437196626
Wazen Shbair <wazen.shbair@gmail.com>
2019-02-20 17:59:14 +0100

    Update GenerateStaticNode.sh

--- Blockchain/ethereum/GenerateStaticNode.sh
+++ Blockchain/ethereum/GenerateStaticNode.sh
@@ -6,5 +6,7 @@ if [ -z "$ip_addr" ]
   ip_addr=`ifconfig eth1 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 fi
 
-geth --datadir=/home/luxbch/data --rpc --rpcaddr 0.0.0.0 --rpcport "8084" --rpccorsdomain "*" --gasprice 0 --networkid 3576 js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null |grep enode | perl -pe "s/\[\:\:\]/$ip_addr/g" | perl -pe "s/^/\"/; s/\s*$/\"/;"
-
+# old version of geth
+#geth --datadir=/home/luxbch/data --rpc --rpcaddr 0.0.0.0 --rpcport "8084" --rpccorsdomain "*" --gasprice 0 --networkid 3576 js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null |grep enode | perl -pe "s/\[\:\:\]/$ip_addr/g" | perl -pe "s/^/\"/; s/\s*$/\"/;"
+geth --datadir=/home/luxbch/data --rpc --rpcaddr 0.0.0.0 --rpcport "8084" --rpccorsdomain "*" --gasprice 0 --networkid 3576 js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null |grep enode | perl -pe "s/127\.0\.0\.1/$ip_addr/g" | perl -pe "s/^/\"/; s/\s*$/\"/;"
+ 
