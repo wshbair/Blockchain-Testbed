72d00aa27ed69379359fa02c00dcadff7af02c76
Wazen Shbair <wazen.shbair@gmail.com>
2019-02-21 15:24:12 +0100

    Update getBalance.sh

--- Blockchain/ethereum/getBalance.sh
+++ Blockchain/ethereum/getBalance.sh
@@ -1,3 +1,3 @@
 
 #!/bin/bash
-geth --datadir=/home/luxbch/data --rpc --rpcaddr 0.0.0.0 --rpcport "8084" --rpccorsdomain "*" --gasprice 0 --networkid 3576 js <(echo 'console.log(web3.fromWei(eth.getBalance(eth.coinbase), "ether"));') 2>/dev/null
+geth --datadir="/home/luxbch/data" --nodiscover --rpc --rpcport "8084" --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --gasprice 0 --networkid 3576 js <(echo 'console.log(web3.fromWei(eth.getBalance(eth.coinbase), "ether"));') 2>/dev/null
