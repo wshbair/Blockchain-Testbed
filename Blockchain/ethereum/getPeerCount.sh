257a2ccea799675415a6221e0bbf160bd3b59acb
Wazen Shbair <wazen.shbair@gmail.com>
2019-03-11 18:54:13 +0100

    Update getPeerCount.sh

--- Blockchain/ethereum/getPeerCount.sh
+++ Blockchain/ethereum/getPeerCount.sh
@@ -1,3 +1,12 @@
 
 #!/bin/bash
-geth --datadir="/home/luxbch/data" --nodiscover --rpc --rpcport "8084" --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --gasprice 0 --networkid 3576 js <(echo 'console.log(net.peerCount);') 2>/dev/null
+#geth --datadir="/home/luxbch/data" --nodiscover --rpc --rpcport "8084" --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --gasprice 0 --networkid 3576 js <(echo 'console.log(net.peerCount);') 2>/dev/null
+function jsonval {
+temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
+    echo ${temp##*|}
+}
+
+json=`curl -H "Content-Type: application/json" --silent --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":74}' localhost:8084`
+prop='result'
+count =`jsonval`
+ echo $count 
