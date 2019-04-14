700b342cd5ad31af52d63684c478ed289621e39c
Wazen Shbair <wazen.shbair@gmail.com>
2019-04-14 12:24:01 +0200

    Update workloads.sh

--- Blockchain/ethereum/workloads.sh
+++ Blockchain/ethereum/workloads.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #echo "Compile the workload generator"
+CompileSmartContract()
+{
 cd Blockchain-Testbed/Blockchain/ethereum/smallbank 
-var = $(make)
+ local v=$(make) 2> /dev/null 2>&1 &
 echo "Done"
+}
+CompileSmartContract
