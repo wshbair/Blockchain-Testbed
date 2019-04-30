5cc205a40587616ffd9b548d6e056925d9fe233d
Wazen Shbair <wazen.shbair@gmail.com>
2019-04-30 15:14:35 +0200

    Update install.sh

--- Blockchain/ethereum/ioheavy/install.sh
+++ Blockchain/ethereum/ioheavy/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 nohup apt-get install npm  2> /dev/null 2>&1 &
 nohup npm install web3@0.19 2> /dev/null 2>&1 &
-node /root/Blockchain-Testbed/Blockchain/ethereum/ioheavy/deploy.js
+#node /root/Blockchain-Testbed/Blockchain/ethereum/ioheavy/deploy.js
