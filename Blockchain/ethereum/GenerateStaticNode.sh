ff9f0502ef57c655bf54461783506be25eeb6d16
Wazen Shbair <wazen.shbair@gmail.com>
2020-10-22 16:57:14 +0200

    Update GenerateStaticNode.sh

--- Blockchain/ethereum/GenerateStaticNode.sh
+++ Blockchain/ethereum/GenerateStaticNode.sh
@@ -2,10 +2,10 @@
 # run on the root node to generate static-nodes.json file
 # old ubuntu 14
 #ip_addr=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
-ip_addr = `ip -4 addr show eno1 | grep -oP "(?<=inet ).*(?=/)"`
+ip_addr=`ip -4 addr show eno1 | grep -oP "(?<=inet ).*(?=/)"`
 if [ -z "$ip_addr" ]
  then
-  ip_addr = `ip -4 addr show eno2 | grep -oP "(?<=inet ).*(?=/)"`
+  ip_addr=`ip -4 addr show eno2 | grep -oP "(?<=inet ).*(?=/)"`
 fi
 
 # old version of geth
