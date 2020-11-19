881686a85768a77f08de5c1e27a22d451cd3a23e
Wazen Shbair <wazen.shbair@gmail.com>
2020-11-19 10:50:03 +0100

    Update GenerateStaticNode.sh

--- Blockchain/ethereum/GenerateStaticNode.sh
+++ Blockchain/ethereum/GenerateStaticNode.sh
@@ -6,14 +6,12 @@ if [ -z "$ip_addr" ]
   ip_addr=`ip -4 addr show eno2 | grep -oP "(?<=inet ).*(?=/)"`
 fi
 
-geth --datadir new-node account --password <(echo -n "") account new
+nohup geth --datadir new-node account new --password <(echo -n "") 2>/dev/null
 bootnode --genkey=nodekey
 cp nodekey new-node
 
 bootnode --nodekey=new-node/nodekey --writeaddress > new-node/enode
-enode =`cat new-node/enod`
-port ="@$ip_addr?discport=0&raftport=50000""
+enode=`cat new-node/enode`
+echo $enode
+port="@$ip_addr?discport=0&raftport=50000"
 echo $enode$port
-
-#geth --datadir=/root/luxbch/data --rpc --rpcaddr 0.0.0.0 --rpcport "8084" --rpccorsdomain "*" --gasprice 0 --networkid 3576 js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null |grep enode | perl -pe "s/127\.0\.0\.1/$ip_addr/g" | perl -pe "s/^/\"/; s/\s*$/\"/;"
- 
