1466a16f97208ba2e423aa7926ffb7c5cb59ba29
Wazen Shbair <wazen.shbair@gmail.com>
2020-11-19 09:49:28 +0100

    Update init.sh

--- Blockchain/ethereum/init.sh
+++ Blockchain/ethereum/init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+ geth --datadir new-node account new
+ bootnode --genkey=nodekey
+ cp nodekey new-node
+
+
+
 #Set Genensis File
-ETH_DATA='/root/Blockchain-Testbed/Blockchain/ethereum/'
-geth --datadir="/root/luxbch/data" init $ETH_DATA"CommonGenesis.json"
-geth --datadir="/root/luxbch/data" --password <(echo -n "") account new
+#ETH_DATA='/root/Blockchain-Testbed/Blockchain/ethereum/'
+#geth --datadir="/root/luxbch/data" init $ETH_DATA"CommonGenesis.json"
+#geth --datadir="/root/luxbch/data" --password <(echo -n "") account new
