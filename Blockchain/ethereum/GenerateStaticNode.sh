3e436a608cb02f1ef8b8b6b78666498ff8dc8c86
Wazen Shbair <wazen.shbair@gmail.com>
2020-11-19 21:56:47 +0100

    Update GenerateStaticNode.sh

--- Blockchain/ethereum/GenerateStaticNode.sh
+++ Blockchain/ethereum/GenerateStaticNode.sh
@@ -6,11 +6,11 @@ if [ -z "$ip_addr" ]
   ip_addr=`ip -4 addr show eno2 | grep -oP "(?<=inet ).*(?=/)"`
 fi
 
-nohup geth --datadir new-node account new --password <(echo -n "") 2>/dev/null
-bootnode --genkey=nodekey
+nohup /root/quorum/build/bin/geth --datadir new-node account new --password <(echo -n "") 2>/dev/null
+/root/quorum/build/bin/bootnode --genkey=nodekey
 cp nodekey new-node
 
-bootnode --nodekey=new-node/nodekey --writeaddress > new-node/enode
+/root/quorum/build/bin/bootnode --nodekey=new-node/nodekey --writeaddress > new-node/enode
 enode=`cat new-node/enode`
 port="@$ip_addr?discport=0&raftport=50000"
 echo $enode$port
