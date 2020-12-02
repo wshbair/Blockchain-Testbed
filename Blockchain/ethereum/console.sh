9350e1a0ac1c4d0acaa5ba87c52fe41ac8fbd91f
Wazen Shbair <wazen.shbair@gmail.com>
2020-12-02 18:41:16 +0100

    Update console.sh

--- Blockchain/ethereum/console.sh
+++ Blockchain/ethereum/console.sh
@@ -2,4 +2,4 @@
 #Open Console window
 #nohup geth --syncmode "full" --allow-insecure-unlock --datadir="/root/luxbch/data" --nodiscover --rpc --rpcport "8084" --rpccorsdomain "*" --rpcapi "admin,eth,debug,miner,net,txpool,personal,web3" --gasprice 0 --maxpeers 32 --networkid 3576 --unlock 0 --password <(echo -n "") 2> /dev/null 2>&1 &
 
-PRIVATE_CONFIG=ignore nohup /root/quorum/build/bin/geth --datadir /root/new-node --nodiscover --verbosity 5 --networkid 31337 --raft --raftport 50000 --rpc --rpcaddr 0.0.0.0 --rpcport 22000 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --emitcheckpoints --port 21000 >> node.log 2>&1 &
+PRIVATE_CONFIG=/root/new-node-1t/tm.ipc nohup /root/quorum/build/bin/geth --datadir /root/new-node --nodiscover --verbosity 5 --networkid 31337 --raft --raftport 50000 --rpc --rpcaddr 0.0.0.0 --rpcport 22000 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --emitcheckpoints --port 21000 >> node.log 2>&1 &
