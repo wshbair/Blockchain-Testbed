3888af2d0b4f9110c7617d04aa74af2ba7eafbb8
Wazen Shbair <wazen.shbair@gmail.com>
2020-12-02 21:44:17 +0100

    Update tessera.sh

--- Blockchain/ethereum/tessera.sh
+++ Blockchain/ethereum/tessera.sh
@@ -1,4 +1,5 @@
-mkdir new-node-1t
-cd new-node-1t
-java -jar /root/tessera.jar -keygen -filename new-node-1
+mkdir /root/new-node-1t
+cd /root/new-node-1t
+java -jar /root/tessera.jar -keygen -filename /root/new-node-1t/new-node-1
+cd /root/Blockchain-Testbed/Blockchain/ethereum
 java -jar /root/tessera.jar -configfile config.json >> tessera.log 2>&1 &
