47cb19832a76f8a18b47cbee3e3dd7106852b4ed
Wazen Shbair <wazen.shbair@gmail.com>
2020-12-15 01:00:11 +0100

    Update GenerateStaticNode.sh

--- Blockchain/ethereum/GenerateStaticNode.sh
+++ Blockchain/ethereum/GenerateStaticNode.sh
@@ -1,11 +1,9 @@
+
 #!/bin/bash
 # run on the root node to generate static-nodes.json file
 #ip_addr=`ip -4 addr show eno1 | grep -oP "(?<=inet ).*(?=/)"`
 ip_addr=`hostname -I`
-if [ -z "$ip_addr" ]
- then
-  ip_addr=`ip -4 addr show eno2 | grep -oP "(?<=inet ).*(?=/)"`
-fi
+ip=`echo $ip_addr | sed -e 's/^[[:space:]]*//'`
 
 nohup /root/quorum/build/bin/geth --datadir new-node account new --password <(echo -n "") >/dev/null 2>&1
 /root/quorum/build/bin/bootnode --genkey=nodekey
@@ -13,5 +11,4 @@ cp nodekey new-node
 
 /root/quorum/build/bin/bootnode --nodekey=new-node/nodekey --writeaddress > new-node/enode
 enode=`cat new-node/enode`
-port="@$ip_addr:21000?discport=0&raftport=50000"
-printf \"enode://$enode$port\"
+printf \"enode://$enode@$ip:21000?discport=0&raftport=50000\"
